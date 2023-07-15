5442c5aded4095429adcf3cc9594c6114169ff2c
turyng <turyng@proton.me>
2023-07-15 11:56:09 +0200

    Improve about section visuals

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,11 +1,11 @@
-import { Grid } from '@mui/material'
+import { Grid, Typography } from '@mui/material'
 import React from 'react'
 
 function About() {
   return (
     <>
       <Grid item className='form-item'>
-        About
+        <Typography variant="h4">About</Typography>
       </Grid>
     </>
   )
