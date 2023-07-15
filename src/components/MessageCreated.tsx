5aae55b84284a772d416ded302958f3affc98c10
turyng <turyng@proton.me>
2023-07-15 11:57:05 +0200

    - Improve Message Created visuals

--- src/components/MessageCreated.tsx
+++ src/components/MessageCreated.tsx
@@ -1,26 +1,26 @@
 import { useLocation } from "react-router-dom";
 import Utils from "../util/Utils";
-import { Box, Grid } from '@mui/material'
+import { Alert, Box, Grid } from '@mui/material'
 import CopyableField from "./CopyableField";
+
 
 
 function MessageCreated() {
   const location = useLocation();
   const url = `http://localhost:5173/messages/${location.state.messageId}?pw=${Utils.toUrlSafeBase64(location.state.password)}`
-  
-  const onCopy = () => {
-    
-    
-  }
   return (
     <>
       <Grid item className='form-item'>
-        <h2>
-          Message successfully created! Copy the link below:
-        </h2>
-        <Box>
-          <CopyableField color="success" focused fullWidth label="URL" id="url" size="medium" multiline defaultValue={url} onCopy={onCopy} />
-        </Box>
+        
+          <Alert severity="success" sx={{ mb: 3 }}>
+
+            Message successfully created! <strong>Copy the link below...</strong>
+          </Alert>
+          <Box>
+            <CopyableField color="success" focused fullWidth label="URL" id="url" size="medium" multiline defaultValue={url} />
+          </Box>
+        
+
       </Grid>
 
     </>
