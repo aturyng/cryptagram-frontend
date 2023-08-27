0ccddf02f8cb013e71c63884716ec6e347b70074
aturyng <turyng@proton.me>
2023-08-27 17:24:52 +0000

    Add basic description of the tool in the about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,6 +6,11 @@ function About() {
       <Grid item className='form-item'>
         <Typography variant="h4">About</Typography>
       </Grid>
+      <Grid item className='form-item'>
+        <Typography variant="h6">This free-of-charge service allows you to securely send an encrypted message, 
+        which self-destructs after it's been read. Also we offer a unique feature to set a time period to view the message. 
+        When the time runs out, the message is removed in real time from the screen. </Typography>
+      </Grid>
     </>
   )
 }
