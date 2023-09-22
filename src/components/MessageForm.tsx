0033da22bc9cd76b27f3a5cfd57565f67c737342
aturyng <turyng@proton.me>
2023-09-22 19:38:47 +0000

    - Make error messages translatable - Wrap additional settings of message sending form in an accordion

--- src/components/MessageForm.tsx
+++ src/components/MessageForm.tsx
@@ -1,4 +1,4 @@
-import { Button, FormControlLabel, Grid, List, ListItem, Switch, TextField, Typography } from '@mui/material'
+import { Accordion, AccordionDetails, AccordionSummary, Button, FormControlLabel, Grid, List, ListItem, Switch, TextField, Typography } from '@mui/material'
 import { Textarea } from '@mui/joy'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import * as Yup from 'yup';
@@ -7,6 +7,8 @@ import axios from 'axios';
 import EncryptionService from '../services/EncryptionService';
 import HttpsIcon from '@mui/icons-material/Https';
 import { useTranslation } from 'react-i18next';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { useState } from 'react';
 
 
 type FormData = {
@@ -29,26 +31,26 @@ export default function MessageForm(props: Props) {
   const { t } = useTranslation();
   const validationSchema = Yup.object().shape({
     body: Yup.string()
-      .required('Message is required')
-      .max(5000, 'Message must not exceed 5000 characters'),
+      .required(t('create-message.form.validation.is-required'))
+      .max(5000, t('create-message.form.validation.message-not-longer-than')),
     destroyLive: Yup.bool(),
     destroyLiveAfterSeconds: Yup.number()
       .when('destroyLive', {
         is: true,
-        then: (schema) => schema.required('Is required')
+        then: (schema) => schema.required(t('create-message.form.validation.is-required'))
       })
-      .min(0, 'Cannot be negative')
-      .max(60, 'Cannot exceed 60 seconds'),
+      .min(0, t('create-message.form.validation.cannot-negative'))
+      .max(60, t('create-message.form.validation.cannot-exceed-sec')),
     password: Yup.string(),
     confirmPassword: Yup.string()
       .when('password', {
         is: (value: string) => value && value.length > 0,
-        then: (schema) => schema.required('Confirm the password')
+        then: (schema) => schema.required(t('create-message.form.validation.confirm-pass'))
       })
-      .oneOf([Yup.ref('password')], 'Passwords do not match'),
+      .oneOf([Yup.ref('password')], t('create-message.form.validation.pass-mismatch')),
     destroyAfterDays: Yup.number()
-      .min(1, 'Cannot be negative or zero')
-      .max(60, 'Cannot exceed 60 days')
+      .min(1, t('create-message.form.validation.must-gt-zero'))
+      .max(60, t('create-message.form.validation.cannot-exceed-days'))
   });
 
   const {
@@ -75,6 +77,14 @@ export default function MessageForm(props: Props) {
   }
 
 
+  const [expanded, setExpanded] = useState<string | false>(false);
+
+  const handleChange =
+    (panel: string) => (_event: React.SyntheticEvent, isExpanded: boolean) => {
+      setExpanded(isExpanded ? panel : false);
+    };
+
+
   return (
 
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -94,72 +104,87 @@ export default function MessageForm(props: Props) {
           </Typography>
         </Grid>
 
-        <Grid container item spacing={3} md={12} xl={6} >
-          <Grid item xs={12} sm={6} xl={8} >
+        <Grid item xs={12}>
+          <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1bh-content"
+              id="panel1bh-header"
+            >
+              <Typography sx={{ width: '33%', flexShrink: 0 }}>
+                {t('create-message.form.additional-settings-accordion.label')}
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Grid container item spacing={3} md={12} xl={6} >
 
-            <List disablePadding>
-              <ListItem alignItems="flex-start" disablePadding>
-                <FormControlLabel label={t('create-message.form.close-after.label')} labelPlacement="end" control={
-                  <Switch {...register("destroyLive")} />}
-                />
-                <TextField
-                  type='number'
-                  disabled={!watch('destroyLive')}
-                  label={t('create-message.form.seconds.label')}
-                  variant='standard'
-                  fullWidth
-                  {...register("destroyLiveAfterSeconds")}
-                  error={errors.destroyLiveAfterSeconds ? true : false}
-                  helperText={errors.destroyLiveAfterSeconds?.message}
-                />
-              </ListItem>
-            </List>
+                <Grid item xs={12} sm={6} xl={8} >
 
-          </Grid>
-          <Grid item xs={12} sm={6} xl={4}  >
-            <TextField
-              type='number'
-              label={t('create-message.form.destroy-after.label')}
-              variant='standard'
-              defaultValue={1}
-              fullWidth
-              {...register("destroyAfterDays")}
-              error={errors.destroyAfterDays ? true : false}
-              helperText={errors.destroyAfterDays?.message}
-            />
-          </Grid>
+                  <List disablePadding>
+                    <ListItem alignItems="flex-start" disablePadding>
+                      <FormControlLabel label={t('create-message.form.close-after.label')} labelPlacement="end" control={
+                        <Switch {...register("destroyLive")} />}
+                      />
+                      <TextField
+                        type='number'
+                        disabled={!watch('destroyLive')}
+                        label={t('create-message.form.seconds.label')}
+                        variant='standard'
+                        fullWidth
+                        {...register("destroyLiveAfterSeconds")}
+                        error={errors.destroyLiveAfterSeconds ? true : false}
+                        helperText={errors.destroyLiveAfterSeconds?.message}
+                      />
+                    </ListItem>
+                  </List>
+
+                </Grid>
+                <Grid item xs={12} sm={6} xl={4}  >
+                  <TextField
+                    type='number'
+                    label={t('create-message.form.destroy-after.label')}
+                    variant='standard'
+                    defaultValue={1}
+                    fullWidth
+                    {...register("destroyAfterDays")}
+                    error={errors.destroyAfterDays ? true : false}
+                    helperText={errors.destroyAfterDays?.message}
+                  />
+                </Grid>
+              </Grid>
+              <Grid container item spacing={3} md={12} xl={6} >
+                <Grid item xs={12} sm={6} >
+                  <TextField
+                    type='password'
+                    label={t('create-message.form.password.label')}
+                    variant='standard'
+                    fullWidth
+                    {...register("password")}
+                    error={errors.password ? true : false}
+                    helperText={errors.password?.message}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6} >
+                  <TextField
+                    type='password'
+                    label={t('create-message.form.confirm-password.label')}
+                    variant='standard'
+                    fullWidth
+                    {...register("confirmPassword")}
+                    error={errors.confirmPassword ? true : false}
+                    helperText={errors.confirmPassword?.message}
+                  />
+                </Grid>
+
+              </Grid>
+            </AccordionDetails>
+          </Accordion>
         </Grid>
-        <Grid container item spacing={3} md={12} xl={6} >
-          <Grid item xs={12} sm={6} >
-            <TextField
-              type='password'
-              label={t('create-message.form.password.label')}
-              variant='standard'
-              fullWidth
-              {...register("password")}
-              error={errors.password ? true : false}
-              helperText={errors.password?.message}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6} >
-            <TextField
-              type='password'
-              label={t('create-message.form.confirm-password.label')}
-              variant='standard'
-              fullWidth
-              {...register("confirmPassword")}
-              error={errors.confirmPassword ? true : false}
-              helperText={errors.confirmPassword?.message}
-            />
-          </Grid>
 
-        </Grid>
         <Grid item xs={12}>
           <Button className="line" variant="contained" type='submit' endIcon={<HttpsIcon />}>{t('create-message.form.btn_confirm.label')}</Button>
         </Grid>
       </Grid>
-
-
 
     </form>
   )
