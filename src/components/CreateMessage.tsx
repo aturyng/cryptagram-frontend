c31297aaf404edf091b954ccc510fff560be796f
aturyng <turyng@proton.me>
2023-09-23 13:16:57 +0000

    Add tooltip next to "New Message" header

--- src/components/CreateMessage.tsx
+++ src/components/CreateMessage.tsx
@@ -1,8 +1,9 @@
-import { Typography } from '@mui/material'
+import { Grid, IconButton, Tooltip, Typography } from '@mui/material'
 import MessageForm from './MessageForm'
 import { useNavigate } from 'react-router-dom';
 import EncryptionService from '../services/EncryptionService';
 import { useTranslation } from 'react-i18next';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 
 function CreateMessage() {
   const navigate = useNavigate();
@@ -13,9 +14,23 @@ function CreateMessage() {
   }
   return (
     <>
-      <Typography variant="h4" align="center" sx={{ mb: 2 }}>
-      {t('create-message.header')}
-      </Typography>
+      <Grid container direction="row" alignItems="center" justifyContent="center" sx={{ mb: 2}}>
+      <Grid item >
+        <Typography variant="h4" align="center" >
+          {t('create-message.header.label')}
+        </Typography>
+      </Grid>
+      <Grid item sx={{ ml: 1}}>
+      <Tooltip title={t('create-message.header.tooltip')}>
+      <IconButton>
+        <HelpOutlineIcon/>
+      </IconButton>
+    </Tooltip>
+      
+      </Grid>
+    </Grid>
+      
+      
       <MessageForm onMessageCreated={howUrl} encryptionService={new EncryptionService} />
 
     </>
